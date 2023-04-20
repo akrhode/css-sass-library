73f2e59ef7e3bf7c04369a8dd4919964494983b7
akrhode <a.rhode@web.de>
2023-04-20 20:59:54 +0200

    Setup project structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,12 @@ const sass = require("gulp-sass")(require("sass"));
 
 //function to compile SASS
 function buildStyles() {
-  return src("index.scss").pipe(sass()).pipe(dest("css"));
+  return src("app/**/*.scss").pipe(sass()).pipe(dest("css"));
 }
 
 //function to watch the SASS file to recompile upon changes
 function watchTask() {
-  watch(["index.scss"], buildStyles);
+  watch(["*.scss"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);

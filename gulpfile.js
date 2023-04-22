5f14b3296c12fefe46d7bf9c68e44c03d04a4e7c
akrhode <a.rhode@web.de>
2023-04-22 14:35:22 +0200

    Use purgecss to remove unused CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 const { src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-
+const purgecss = require("gulp-purgecss");
 //function to compile SASS
 function buildStyles() {
-  return src("app/**/*.scss").pipe(sass()).pipe(dest("css"));
+  return src("app/**/*.scss")
+    .pipe(sass())
+    .pipe(purgecss({ content: ["*.html"] }))
+    .pipe(dest("css"));
 }
 
 //function to watch the SASS file to recompile upon changes
 function watchTask() {
-  watch(["*.scss"], buildStyles);
+  watch(["*.scss", "*.html"], buildStyles);
 }
 
 exports.default = series(buildStyles, watchTask);
